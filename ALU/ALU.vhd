5c5757255e28eca28f6c1cf111bd9d949f891245
Radwa Khattab <RadwaSM@hotmail.com>
2019-12-22 00:49:35 +0200

    with ZF and enable

--- ALU/ALU.vhd
+++ ALU/ALU.vhd
@@ -2,11 +2,11 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity ALU is
-    port (A, B          :       in std_logic_vector (15 downto 0);
-          F             :       out std_logic_vector (15 downto 0);
+    port (A, B             :       in std_logic_vector (15 downto 0);
+          F                :       out std_logic_vector (15 downto 0);
           NopA,ADDD,ADC,SUB,SBC,ANDD,ORR,XNORR,IncA,DecA,Clear,NotA,LSR_B,ROR_B,RRC_B,ASR_B,LSL_B,ROL_B,RLC_B : in std_logic;
-          Cin       :       in std_logic;
-          Cout, ZF      :       out std_logic);
+          Cin, ZFin, en    :       in std_logic;
+          Cout, ZFout      :       out std_logic);
 end entity ALU;
 
 architecture ALU_arch of ALU is
@@ -17,8 +17,17 @@ component full16bitAdder is
 		  Cout      :   OUT  std_logic);
 end component full16bitAdder;
 
+component mux4x1 is
+    port (in3, in2, in1, in0  : std_logic;
+          s1, s0       : std_logic;
+          f       : out std_logic);
+end component mux4x1;
+
+
 signal NotB, NegB, FA1, FAB, FABC, FANegB, FANegBNegC, FANeg1, Fout    : std_logic_vector (15 downto 0);   
 signal CB, NotC, CA1, CAB, CABC, CANegB, CANegBNegC, CANeg1      : std_logic;
+signal ZFMout, oring, tempCout   :   std_logic;
+
 
 begin
     NotB <= not B;
@@ -34,6 +43,7 @@ begin
     SUB_Adder    : full16bitAdder  port map (A, NegB, '0', FANegB, CANegB);
     SBC_Adder_C0 : full16bitAdder  port map (FANegB, "1111111111111111", CANegB, FANegBNegC, CANegBNegC); 
     
+    ZFMux        : mux4x1 port map ('0', '1', ZFin, ZFin, en, oring, ZFMout);
     
     process (NopA,ADDD,ADC,SUB,SBC,ANDD,ORR,XNORR,IncA,DecA,Clear,NotA,LSR_B,ROR_B,RRC_B,ASR_B,LSL_B,ROL_B,RLC_B)
     begin
@@ -41,16 +51,16 @@ begin
             Fout    <= A;
         elsif ADDD = '1' then
             Fout    <= FAB;
-            Cout <= CAB;
+            tempCout <= CAB;
         elsif ADC = '1' then
             Fout    <= FABC;
-            Cout <= CABC;
+            tempCout <= CABC;
         elsif SUB = '1' or (SBC = '1' and Cin = '1') then
             Fout    <= FANegB;
-            Cout <= CANegB;
+            tempCout <= CANegB;
         elsif SBC = '1' then
             Fout    <= FANegBNegC;
-            Cout <= CANegBNegC;
+            tempCout <= CANegBNegC;
         elsif ANDD = '1' then
             Fout <= A and B;
         elsif ORR = '1' then
@@ -59,10 +69,10 @@ begin
             Fout <= A xnor B;
         elsif IncA = '1' then
             Fout    <= FA1;
-            Cout <= CA1;
+            tempCout <= CA1;
         elsif DecA = '1' then
             Fout    <= FANeg1;
-            Cout <= CANeg1;
+            tempCout <= CANeg1;
         elsif Clear = '1' then
             Fout <= (others => '0');
         elsif NotA = '1' then
@@ -84,7 +94,16 @@ begin
         end if;
     end process;
     
-    F <= Fout;
-    Zf <= Fout(0) or Fout(1) or Fout(2) or Fout(3) or Fout(4) or Fout(5) or Fout(6) or Fout(7)
-        or Fout(8) or Fout(9) or Fout(10) or Fout(11) or Fout(12) or Fout(13) or Fout(14) or Fout(15);
+    process (en)
+    begin
+        if en = '1' then
+            F <= Fout;
+            Cout <= tempCout;
+            oring <= Fout(0) or Fout(1) or Fout(2) or Fout(3) or Fout(4) or Fout(5) or Fout(6) or Fout(7)
+                or Fout(8) or Fout(9) or Fout(10) or Fout(11) or Fout(12) or Fout(13) or Fout(14) or Fout(15);
+        end if;
+    end process;
+    
+    ZFout <= ZFMout;
+    
 end ALU_arch;
